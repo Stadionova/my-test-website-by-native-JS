cc64bc5c98409127c1d99a0d50d4f358a472ad59
Stadionova <726383@mail.ru>
2019-10-19 12:01:51 +0300

    added container

--- src/App.js
+++ src/App.js
@@ -7,9 +7,11 @@ function App() {
   return (
     // для всего сайта создала общий div,
     // в котором будут лежать все декомпозированные компоненты
-    <div className="grid__App">
-      <Header />
-      <Main />
+    <div className='container'>
+      <div className="grid__App">
+        <Header />
+        <Main />
+      </div>
     </div>
   );
 }
