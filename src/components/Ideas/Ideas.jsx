1c9d6304ea38cdac42b5bfedbd27966d810a90e1
Stadionova <726383@mail.ru>
2019-10-22 13:20:44 +0300

    added state, now event & state are working both correctly

--- src/components/Ideas/Ideas.jsx
+++ src/components/Ideas/Ideas.jsx
@@ -4,13 +4,20 @@ import './Ideas.css';
 // класс Ideas наследует от Component
 class Ideas extends Component {
 
+    // создаём конструктор, который обязательно должен наследовать от Component
+    // этот constructor должен внутри себя вызвать конструктор супер-класса/конструктора-родителя Component, от которого он наследовался
+    // наследует он при помощи метода super
     constructor() {
 
-        super();
+        super(); // это вызов родителя
 
-        this.state = {
+        // дальше объявлеям свойство state
+        // то есть, наша компонента обладает каким-то внешним видом, поведением и состоянием: state (то есть обладает значением каких-то полей)
+        this.state = { idea: 'first idea' }; // например компонента обладает идеей со значением: first idea
+        // делается это для того, чтобы использовать этот стэйт где-либо по коду и не инлайнить значения вручную
+        // использую этот стэйт ниже в методе render
+        // state пишем в фигурных скобках, если используем в jsx
 
-        };
     }
 
     // метод createNewTask, который создаёт новые таски
@@ -23,8 +30,14 @@ class Ideas extends Component {
     render() {
         return (
             // добавила обработчик события на клик по кнопке
-            <div className='clickButton'>
-                <button onClick={this.createNewTask}>Submit new Idea</button>
+            <div>
+                <div className='clickButton'>
+                    <button onClick={this.createNewTask}>Submit new Idea</button>
+                </div>
+                <div className='ideaState'>
+                    {/* достаю значение из state  */}
+                    Idea: <span>{this.state.idea}</span>
+                </div>
             </div>
         );
     }
