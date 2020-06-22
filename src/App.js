83b7d81cb78992625d37b1d973dfdbdb52704a5a
Yulia Tribunskaya <ytribunskaya@kameleoon.com>
2020-06-22 20:29:33 +0300

    test commit

--- src/App.js
+++ src/App.js
@@ -8,51 +8,55 @@ import Competition from './components/Competition/Competition';
 // импортирую объект articles
 import { articles } from './components/fixture';
 // import { BrowserRouter, Route } from 'react-router-dom';
-
+console.log(1);
 // function App() {
 class App extends Component {
-  constructor() {
+    constructor() {
 
-    super();
+        super();
 
-    this.state = {
-      ideas: []
-    };
+        this.state = {
+            ideas: []
+        };
 
-    this.addIdeaToState = this.addIdeaToState.bind(this);
+        this.addIdeaToState = this.addIdeaToState.bind(this);
 
-  }
+    }
 
-  // общий метод добавления идеи
-  addIdeaToState(idea) {
-    // создаю переменную, в которую кладу массив с идеями из стэйта
-    const newIdea = this.state.ideas;
-    // пушу в этот массив новую переданную идею
-    newIdea.push(idea);
-    // кладу новый массив с новой идеей обратно в стэйт вместо предыдущего массива
-    this.setState({ ideas: newIdea })
-  }
+    // общий метод добавления идеи
+    addIdeaToState(idea) {
+        // создаю переменную, в которую кладу массив с идеями из стэйта
+        const newIdea = this.state.ideas;
+        // пушу в этот массив новую переданную идею
+        newIdea.push(idea);
+        // кладу новый массив с новой идеей обратно в стэйт вместо предыдущего массива
+        this.setState({ ideas: newIdea })
+    }
 
-  render() {
-    // у меня в articles лежит массив объектов, достаю его первый элемент по индексу 0
-    // и кладу первый элемент массива в переменную article
-    const article = articles[0];
-    const twoObj = articles[1];
-    console.log(twoObj);
-    return (
-      <div className='container'>
-        <div className="grid__App">
-          {/* передаю в пропсы компоненты Header атрибут article, в который кладу переменную article */}
-          <Header article={article} />
-          <Main ideas={this.state.ideas} onAddIdeaToState={this.addIdeaToState} />
-          {/* в пропс компоненты Ideas передаю пропс ideas, в котором лежит массив с идеями, название пропсам даю любое */}
-          {/* так же передаю в пропс общий метод */}
-          <Ideas ideas={this.state.ideas} onAddIdeaToState={this.addIdeaToState} />
-          <Competition objProps={twoObj} />
-        </div>
-      </div>
-    );
-  }
+    render() {
+        // у меня в articles лежит массив объектов, достаю его первый элемент по индексу 0
+        // и кладу первый элемент массива в переменную article
+        const article = articles[0];
+        const twoObj = articles[1];
+        console.log(twoObj);
+        return ( <
+            div className = 'container' >
+            <
+            div className = "grid__App" > { /* передаю в пропсы компоненты Header атрибут article, в который кладу переменную article */ } <
+            Header article = { article }
+            /> <
+            Main ideas = { this.state.ideas }
+            onAddIdeaToState = { this.addIdeaToState }
+            /> { / * в пропс компоненты Ideas передаю пропс ideas, в котором лежит массив с идеями, название пропсам даю любое * / } { / * так же передаю в пропс общий метод * / } <
+            Ideas ideas = { this.state.ideas }
+            onAddIdeaToState = { this.addIdeaToState }
+            /> <
+            Competition objProps = { twoObj }
+            /> < /
+            div > <
+            /div>
+        );
+    }
 }
 
 export default App;
